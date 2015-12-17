de1171f25eb6b0f58b78f9bc2b6572ac2330eec4
rjolly1971 <raymond.jolly@gmail.com>
2015-12-17 18:38:40 -0500

    Update media.py

--- media.py
+++ media.py
@@ -1,6 +1,9 @@
 import webbrowser
 
 class Movie():
+    #RATING={'G':'G','PG':'PG','R': 'R'}  In this example how could one call this in the entertainment_center.py file??
+    #I know that this is supposed to be a best practice but the material did not cover how this could be called
+    #in from other files.
 
 
     def __init__(self, movie_title, movie_storyline, genre, duration, cast,
